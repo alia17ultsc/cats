e3ddaf65944054386d7aca8dd6285989073a26e0
alia17ultsc <aliafayez2009@gmail.com>
2024-07-13 16:01:26 +0300

    continue switch

---
+++ javascript/script.js
@@ -0,0 +1,25 @@
+var leftarrow=document.getElementById("leftarrow");
+leftarrow.style.opacity = "0.5";
+var catnumber=0;
+var cats=["sonya", "ryzhyk", "felix", "soonie"];
+
+
+function changecat(switchcat){
+    if (catnumber + switchcat <0 || catnumber + switchcat>cats.length){
+        return;
+    }
+    catnumber=catnumber+switchcat;
+    var currentcat=cats[catnumber];
+    var catname=document.getElementById("catname");
+    var catimg=document.getElementById("catimg");
+    
+    
+    if (currentcat=="sonya"){
+        catname.innerText="Sonya";
+        catimg.src="../pictures/sonya.jpg";
+    }
+    if (currentcat=="ryzhyk"){
+       catname.innerText="Ryzhyk";
+        catimg.src="../pictures/ryzhyk.jpg"; 
+    }
+}
