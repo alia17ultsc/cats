03ec7861899b9a61f261415b9e00db2dcaea9f8a
alia17ultsc <aliafayez2009@gmail.com>
2024-07-24 17:14:13 +0300

    continue project

--- javascript/script.js
+++ javascript/script.js
@@ -7,7 +7,10 @@ var cats=["sonya", "ryzhyk", "felix", "soonie"];
 
 function nextpage(){
     if (catnumber==0){
-        window.location.href="allphoto.html";
+        window.location.href="sonya_photo.html";
+    }
+    else if (catnumber==1){
+        window.location.href="ryzhyk_photo.html";
     }
 }
 
@@ -24,18 +27,39 @@ function changecat(switchcat){
     var catname=document.getElementById("catname");
     var catimg=document.getElementById("catimg");
     var catdesc=document.getElementById("catdesc");
+    var catinfo=document.getElementById("catinfo"); 
     
     if (currentcat=="sonya"){
         catname.innerText="Sonya";
         catimg.src="../pictures/sonya.jpg";
         catdesc.innerText="Sonya was born on February 10th of the year 2023. She is a queen.";
+        catinfo.innerText="Egyptians believed cats were magical creatures, capable of bringing good luck to the people who housed them.";
         
     }
-    if (currentcat=="ryzhyk"){
+    else if (currentcat=="ryzhyk"){
        catname.innerText="Ryzhyk";
         catimg.src="../pictures/ryzhyk.jpg"; 
         catdesc.innerText="In the year 2022 he started living a home. Every evening he controls the cat restaurant. He doesnt like strangers.";
+        catinfo.innerText="If you thought cats spent a lot of their lives sleeping, you’d be right. According to Veterinary Hub, Cats actually spend 70% of their lives sleeping, which works out to around 13-16 hours a day. It’s a cat’s life!";
     }
+    
+    else if (currentcat=="felix"){
+       catname.innerText="Felix";
+        catimg.src="../pictures/felix.jpg"; 
+        catdesc.innerText="Felix 8 years old";
+        catinfo.innerText="You’ve heard of monkeys and dogs in space, but did you know that a cat braved the great unknown too? On October 18th 1963 Felicette, also known as ‘Astrocat’ was the first and only cat to go to space.";
+    }
+    
+    else if (currentcat=="soonie"){
+       catname.innerText="Soonie";
+        catimg.src="../pictures/soonie.jpg"; 
+        catdesc.innerText="This cat lives with Judy in Sharm";
+        catinfo.innerText="Cats can see spirits and according of its energy they can scare away them or to enhance their presence in our benefit.";
+    }
+    
+    
+    
+    
     if (catnumber==0){
         leftarrow.style.opacity="0.5";
         rightarrow.style.opacity="1";
